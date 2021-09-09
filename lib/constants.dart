7e64e922492a1ca9713a850184dbee9caab96a94
DongheonKim <oueya1479@naver.com>
2021-09-09 23:25:37 +0900

    Create LoginPage

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFEE5243);
+const kBackgroundColor = Color(0xFFF4E6CC);
+const kPointColor = Color(0xFFEDAD53);
+const kSubColor = Color(0xFF4F2D2C);
+const kFontColor = Color(0xFF282828);
+
+const kDefaultPadding = 20.0;
